4e67bd650d3cdd79f69e4810684964535af5f95e
Ratnam Sachan <sachanratnam@gmail.com>
2025-12-14 09:43:30 +0000

    replace this image with this https://ik.imagekit.io/kqjunrsbz/signature%

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'www.thesignaturestudio.in',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
