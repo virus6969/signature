e590b180b70f2a4bdafaec30121066c252ca9fb7
Ratnam Sachan <sachanratnam@gmail.com>
2025-12-14 09:38:56 +0000

    recaluculate the percentage of discount we are giving on base porodict f

--- src/components/app/offer-banner.tsx
+++ src/components/app/offer-banner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function OfferBanner() {
   return (
     <div className="bg-accent text-accent-foreground text-center p-2 font-bold animate-pulse">
-      🔥 Limited Time: 50% OFF On All Signature Designs!
+      🔥 Limited Time: 90% OFF On All Signature Designs!
     </div>
   );
 }
