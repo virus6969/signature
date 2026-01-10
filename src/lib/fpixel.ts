0f09c073bf296fc231a3d1fbdd83b1540e075aa7
Ratnam Sachan <sachanratnam@gmail.com>
2026-01-10 11:15:27 +0000

    Try fixing this error: `Runtime Error: Error: window.fbq is not a functi

--- src/lib/fpixel.ts
+++ src/lib/fpixel.ts
@@ -4,10 +4,16 @@
 export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
 
 export const pageview = () => {
+  if (typeof window.fbq !== 'function') {
+    return;
+  }
   (window as any).fbq('track', 'PageView')
 }
 
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const event = (name: string, options = {}) => {
+  if (typeof window.fbq !== 'function') {
+    return;
+  }
   (window as any).fbq('track', name, options)
 }
